f9a7194dd0b76678f86390047a11f60951dd35a3
rbijoor <rbijoor@plansource.com>
2022-09-08 13:28:00 +0530

    react static text

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+
+function Header(){
+  return( <>
+  <div className="card">
+    <h3>This is a note title.</h3>
+    <p>This is note content</p>
+  </div>
+  <hr/>
+  <h1>Hi</h1>
+  </>
+  )
+}
+
+export default Header;
